2922d400501b3199b04a989d6403f43e477ec26b
Felipe Moreira da Cruz <piradous@gmail.com>
2023-08-01 11:07:35 -0300

    style: NameFilter

--- src/style/NameFilterStyle.js
+++ src/style/NameFilterStyle.js
@@ -2,17 +2,13 @@ import styled from 'styled-components';
 
 const NameFilterStyle = styled.input`
 color: black;
-font-size: 16px;
-margin: 0;
+font-size: 14px;
+margin: 0 auto;
 text-align: center;
 line-height: 1;
-width: 100%;
-@media (min-width: 768px) {
-  font-size: calc(16px + 1vw);
-}
-@media (min-width: 1024px) {
-  font-size: calc(16px + 2vw);
-
+width: 30%;
+display: block;
+left: 50%;
 `;
 
 export default NameFilterStyle;
